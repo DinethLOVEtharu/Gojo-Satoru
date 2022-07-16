5907d7e6ec84a744e3dda4abb60529ab19a40d19
DinethLOVEtharu <109384221+DinethLOVEtharu@users.noreply.github.com>
2022-07-16 23:17:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94773840857']
+global.premium = ['94773840857']
+global.ownernomer = '94773840857'
+global.ownername = 'DinethJAY'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
